88735b8144ea553deb2cd6b6fdb96aa87b07a333
Steffen Macke <sdteffen@sdteffen.de>
2013-03-03 15:36:43 +0100

    Standard icon sizes.

--- src/standardicon.sh
+++ src/standardicon.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-import $1
 convert -fuzz 15% -trim \
 -filter cubic -define filter:b=1.3 -define filter:c=1.3 -resize 190x190 \
 -background white -gravity center -extent 200x200 \
--resize 20x20 \
+-resize 22x22 \
 -gamma 0.5 \
 $1 $1
-#-fuzz 15% -transparent white \
-#viewnior $1
