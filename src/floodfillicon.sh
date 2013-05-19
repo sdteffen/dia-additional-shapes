5c9f0015b4e7e2e0663835227d775894a44c84e2
Steffen Macke <sdteffen@sdteffen.de>
2013-05-19 15:10:17 +0200

    Prevent the indexed PNG format from interfering in the floodfilling process.

--- src/floodfillicon.sh
+++ src/floodfillicon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Floodfill all white areas from the outside with transparency
 
-convert $1 -bordercolor white -border 1 +repage  png:- | convert - -alpha on channel RGBA -fuzz 8% -fill none -draw 'color 0,0 floodfill' png:- | convert - -crop '22x22+1+1!' $1
+convert $1 -bordercolor white -border 1 +repage  png32:- | convert - -alpha on channel RGBA -fuzz 8% -fill none -draw 'color 0,0 floodfill' png32:- | convert - -crop '22x22+1+1!' png32:$1
