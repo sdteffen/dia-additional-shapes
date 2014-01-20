ad0a9b86170f68b5e47b8e9421b82f0cac7d7ec5
Steffen Macke <sdteffen@sdteffen.de>
2014-01-20 13:33:26 +0100

    Getting started with JSON conversion.

---
+++ src/shapes2json.cs
@@ -0,0 +1,85 @@
+//
+// shapes2sprite.cs: Convert dia shapes into JSON
+//
+// Author:
+//   Steffen Macke (sdteffen@sdteffen.de)
+//
+// Copyright (C) 2014 Steffen Macke (http://dia-installer.de)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Xml.XPath;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+// JSON data shapes and objects
+public class Shapes2Json
+{
+  public static void Main (string[]args)
+  {
+    if ((args.Length == 1 && ("-h" == args[0] || "--help" == args[0]))
+	|| args.Length > 2)
+      {
+	Console.Error.WriteLine ("USAGE: shapes2json [Options]");
+	Console.Error.WriteLine ("Options:");
+	Console.Error.WriteLine
+	  ("--datadir=datadir			Path where sheets and shapes reside");
+	Console.Error.WriteLine
+	  ("-h, --help                 Display help and exit");
+	Console.Error.WriteLine
+	  ("-v, --version              Display version and exit");
+	return;
+      }
+
+    if (1 == args.Length && ("-v" == args[0] || "--version" == args[0]))
+      {
+	Console.Error.WriteLine ("shapes2json 0.1.0");
+	Console.Error.WriteLine ("Copyright (c) 2011 - 2014 Steffen Macke");
+	return;
+      }
+
+    for (int i = 0; i < args.Length; i++)
+      {
+	if (10 < args[i].Length && "--datadir=" == args[i].Substring (0, 10))
+	  Directory.SetCurrentDirectory (args[i].Substring (10));
+      }
+
+    bool first = true;
+    string o = "";
+
+    Console.WriteLine ("{");
+
+    DiaIcons diaicons = new DiaIcons ();
+    foreach (KeyValuePair < string, string > icon in diaicons.icons)
+    {
+      if (DiaIcons.SHEET_SPECIFIC == icon.Value)
+	continue;
+
+      if(first)
+      {
+        first = false;
+        o = "";
+      }
+      else
+        o = ",";
+      Console.WriteLine(o+"{c:'"+icon.Key.Replace(".png", "")+"'}");
+    }
+    Console.WriteLine("}");
+  }
+}
+
